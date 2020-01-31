a4d8fb2e84ef9cfcd85036f903711af8b79d224a
unknown <percell29@gmail.com>
2020-01-30 19:19:50 -0500

    first time using gitbash

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+name = input('Hello what is your name? ')
+
+print('Hello', name,'lets start a story')
+
+noun = input('Give me a noun. ')
+
+adjective = input('Give me a adjective. ')
+
+verb = input('Give me a verb. ')
+print('The', adjective, noun, verb, 'over the fence.')
